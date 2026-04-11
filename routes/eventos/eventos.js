61b700361fcf2d2e9a5afa54953a6836d0808896
lrsholanda <lrsholanda@gmail.com>
2026-04-11 13:50:08 -0300

    feat: organização das rotas

--- routes/eventos/eventos.js
+++ routes/eventos/eventos.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
+// DADOS MOCK (temporário)
+const eventos = [
+  {
+    nome: "Desafio Tecnológico",
+    slug: "dtec",
+    descricao: "Desafio Tecnológico"
+  },
+  {
+    nome: "Control4Future",
+    slug: "c4f",
+    descricao: "Control for Future"
+  }
+];
+
+
+// LISTA DE MEMBROS
 router.get("/eventos", (req, res) => {
-  res.render("eventos/eventos");
+  res.render("eventos/index", {
+    eventos
+  });
+});
+
+
+// MEMBRO INDIVIDUAL
+router.get("/eventos/:slug", (req, res) => {
+  const slug = req.params.slug;
+
+  const evento = eventos.find(e => e.slug === slug);
+
+  if (!evento) {
+    return res.status(404).send("Evento não encontrado");
+  }
+
+  res.render("eventos/evento", {
+    evento
+  });
 });
 
 module.exports = router;
--- routes/quem-somos/membros/membros.js
+++ routes/quem-somos/membros/membros.js
@@ -5,238 +5,238 @@ const router = express.Router();
 const membros = [
   {
     nome: "Larissa dos Santos Holanda",
-    slug: "larissa",
+    slug: "larissa-holanda",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
-    nome: "Davi Jesse Honorato de Oliveira",
-    slug: "jesse",
+    nome: "Davi Jessé Honorato de Oliveira",
+    slug: "davi-jesse",
     eixo: "Inovação",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Lucas de Oliveira Sobral",
-    slug: "lucas",
+    slug: "lucas-sobral",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
-    nome: "João Vitor Marques Falcão",
-    slug: "falcão",
+    nome: "João Victor Marques Falcão",
+    slug: "joao-victor-falcao",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Osvaldo Medeiros Cavalcante Neto",
-    slug: "osvaldo",
+    slug: "osvaldo-medeiros",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Davi Maurício Guedes Moreira",
-    slug: "maurício",
+    slug: "davi-mauricio",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Lucas Martins Menezes",
-    slug: "lucas",
+    slug: "lucas-martins",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Davi Sousa Trévia Magalhães",
-    slug: "trevia",
+    slug: "davi-trevia",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Gabriel Gonzaga Sá Barreto",
-    slug: "gabriel",
+    slug: "gabriel-gonzaga",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Matheus Rocha Gomes da Silva",
-    slug: "matheus rocha",
+    slug: "matheus-rocha",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Paulo Ricardo Jucá Santiago",
-    slug: "paulo ricardo",
+    slug: "paulo-ricardo",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
-    nome: "Maria Paula Mesquita",
-    slug: "maria paula",
+    nome: "Maria Paula Mesquita Silva Saraiva",
+    slug: "maria-paula-mesquita",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Iago de Farias Gomes",
-    slug: "iago",
+    slug: "iago-farias",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Wagner Silva da Costa Filho",
-    slug: "wagner",
+    slug: "wagner-silva",
     eixo: "Técnico",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
-    nome: "Laiza Edwiges Rocha da Silva",
-    slug: "laiza",
+    nome: "Laiza Edwigens Rocha da Silva",
+    slug: "laiza-edwiges",
     eixo: "Técnico",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Amanda Kelly Caetano da Silva",
-    slug: "amanda",
+    slug: "amanda-kelly",
     eixo: "Apoio",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
-    nome: "Luan Maciel dos Santos",
-    slug: "luan",
+    nome: "Luan José Maciel dos Santos",
+    slug: "luan-maciel",
     eixo: "Apoio",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Bruno Costa Ferreira",
-    slug: "bruno",
+    slug: "bruno-costa",
     eixo: "Apoio",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "João Matheus Pereira da Silva",
-    slug: "joão matheus",
+    slug: "joao-matheus",
     eixo: "Apoio",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Rafael Martins Guimaraes Vieira",
-    slug: "rafael",
+    slug: "rafael-martins",
     eixo: "Apoio",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Savlio Carvalho Pontes",
-    slug: "savlio",
+    slug: "savlio-carvalho",
     eixo: "Apoio",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Matheus Andrade Maia",
-    slug: "matheus maia",
+    slug: "matheus-maia",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Vitor Nogueira de Sousa",
-    slug: "vitor",
+    slug: "vitor-nogueira",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Sophya Evelyn de Sousa Ferreira Silva",
-    slug: "sophya",
+    slug: "sophya-evelyn",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Penelope Aghata Liberato de Oliveira",
-    slug: "penelope",
+    slug: "penelope-aghata",
     eixo: "Inovação",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Joinale Mendes da Rocha",
-    slug: "joinale",
+    slug: "joinale-mendes",
     eixo: "Inovação",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Levi Rocha de Santana Guimares",
-    slug: "levi",
+    slug: "levi-rocha",
     eixo: "Apoio",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "David Baima Monte",
-    slug: "david",
+    slug: "david-baima",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Leticia Romero Ciconini Gomes da Silva",
-    slug: "leticia",
+    slug: "leticia-ciconin",
     eixo: "Apoio",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Gabriel Mapurunga de Carvalho",
-    slug: "gabriel",
+    slug: "gabriel-mapurunga",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Aquiles de Oliveira Ferreira",
-    slug: "aquiles",
+    slug: "aquiles-ferreira",
     eixo: "Inovação",
     time: "Mídias",
     descricao: "blábláblá..."
   },
   {
     nome: "Fernando Caio Ribeiro Moura",
-    slug: "fernando",
-    eixo: "zzz",
-    time: "zzz",
+    slug: "fernando-caio",
+    eixo: null,
+    time: null,
     descricao: "blábláblá..."
   },
   {
     nome: "Geovanna Angelino de Sousa",
-    slug: "geovanna",
+    slug: "geovanna-angelino",
     eixo: "Inovação",
     time: "Processos",
     descricao: "blábláblá..."
   },
   {
     nome: "Camila De Oliveira Silva",
-    slug: "camila",
+    slug: "camila-oliveira",
     eixo: "Apoio",
     time: "Mídias",
     descricao: "blábláblá..."
