2dcd4ebca15110d33b41b10702592486b5fb6626
Kim <camiladezoitimhak@gmail.com>
2026-04-20 17:21:49 -0300

    feat: adiciona artigos e notícias na página de notícias

---
+++ routes/noticias/pagina-de-noticias.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const noticias = [
+    {
+        nome: "Apresentação Institucional do Raitec 2026",
+        slug: "apresentacao-institucional-2026",
+        data: "00/00/0000",
+        descricao: "blábláblá...",
+        conteudo: "blábláblá...",
+    }
+]
---
+++ routes/raipedia/artigo.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const artigos = [
+    {
+      nome: "Capacitação de Arduino",
+      slug: "capacitacao-de-arduino",
+      conteudo: "blábláblá...",
+    },
+    {
+      nome: "Capacitação de SCRUM",
+      slug: "capacitacao-de-scrum",
+      conteudo: "blábláblá...",
+    },
+    {
+      nome: "Capacitação de FLUTTER",
+      slug: "capacitacao-de-flutter",
+      conteudo: "blábláblá...",
+    }
+]
+
